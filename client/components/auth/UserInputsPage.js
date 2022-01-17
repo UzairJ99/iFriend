127fb276011549c35da3855dfb00b2a2d23fb5dd
UzairJ99 <uzair-jawaid@hotmail.com>
2022-01-16 19:40:22 -0500

    added user inputs blank page template and installed firebase package

---
+++ client/components/auth/UserInputsPage.js
@@ -0,0 +1,55 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Button, Input } from 'react-native-elements';
+
+// testing home screen component for navigation stack
+function UserInputsPage(props) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.loginForm}>
+        <Text style={styles.logo}>iFriend</Text>
+        <Button
+          title="Next"
+          raised={true}
+          buttonStyle={styles.nextBtn}
+          containerStyle={{ borderRadius: 15, marginTop: 20 }}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  signupBtn: {
+    fontSize: 30,
+    height: 62,
+    width: 180,
+    borderRadius: 15,
+    backgroundColor: '#2184C4'
+  },
+  nextBtn: {
+    fontSize: 30,
+    height: 62,
+    width: 300,
+    borderRadius: 15,
+    backgroundColor: '#2184C4'
+  },
+  logo: {
+    margin: 20,
+    fontSize: 70,
+    color: '#2184C4'
+  },
+  loginForm: {
+    flex: 3,
+    width: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 100,
+    marginTop: 50
+  }
+});
+
+export default UserInputsPage;
