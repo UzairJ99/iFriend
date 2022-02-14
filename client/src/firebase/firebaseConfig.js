4f7d7e4046cd280dc9c3a6fac26b36900f0578b5
UzairJ99 <uzair-jawaid@hotmail.com>
2022-02-14 15:31:01 -0500

    added requests and friendslist tabs

--- client/src/firebase/firebaseConfig.js
+++ client/src/firebase/firebaseConfig.js
@@ -4,11 +4,7 @@ import "firebase/compat/firestore";
 
 // need to set up dotenv to hide this from github for now, paste from the messenger gc when you need to run the app.
 const firebaseConfig = {
-    apiKey: 'AIzaSyCN_ABhqou46YqjGaXIWzPK3eSMfLRlr8g',
-    authDomain: 'ifriend-45f5e.firebaseapp.com',
-    databaseURL: 'https://ifriend-45f5e-default-rtdb.firebaseio.com/',
-    projectId: 'ifriend-45f5e',
-    appId: '1:477250442772:ios:54d74df4499b2e60bdf901',
+    
 };
 
 
