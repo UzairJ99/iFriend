5676bc54d715613232e6a27e9cfcc5c0d537280c
Olayiwola1 <olayiwolabakare@ymail.com>
2022-02-23 19:26:45 -0500

    settings page: DB functionality complete

--- client/components/user_experience/SettingsPage.js
+++ client/components/user_experience/SettingsPage.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, Alert } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { Button, Input } from 'react-native-elements';
 import { firebase } from '../../src/firebase/firebaseConfig';
 
+// the user id and email would never change, so we can use it for querying purposes and for initial displays
 const SettingsPage = (props) => {
-    const [firstname, setFirstName] = useState(props.route.params.userInfo.firstname);
-    const [lastname, setLastName] = useState(props.route.params.userInfo.lastname);
+    const [firstname, setFirstName] = useState(null);
+    const [lastname, setLastName] = useState(null);
     const [email, setEmail] = useState(props.route.params.userInfo.email);
-    const [username, setUserName] = useState(props.route.params.userInfo.username);
-    const [school, setSchool] = useState(props.route.params.userInfo.schoolname);
+    const [username, setUserName] = useState(null);
+    const [schoolname, setSchoolName] = useState(null);
 
-    // console.log(props)
-    const name = props.route.params.userInfo.username
+    useEffect(() => {
+        firebase.firestore().collection('users').doc(`${props.route.params.userInfo.id}`).get()
+        .then(documentSnapshot => {
+            // console.log('User exists: ', documentSnapshot.exists);
+            if (documentSnapshot.exists) {
+                const userData = documentSnapshot.data();
+                console.log('User data: ', userData);
+                setFirstName(userData.firstname);
+                setLastName(userData.lastname);
+                setUserName(userData.username);
+                setSchoolName(userData.schoolname);
+            }
+            else { 
+                console.log('This user does not exist in the database');
+            }
+        }).catch((error) => {
+            console.log('There was an error while trying to read from the database');
+            return (
+                <>/</>
+            )
+        });
+    }, []);
+
     const saveChangesBtnAlert = () =>
         Alert.alert(
         "Save Changes",
@@ -28,7 +46,14 @@ const SettingsPage = (props) => {
             },
             { 
                 text: "OK", 
-                onPress: () => {}, 
+                onPress: () => firebase.firestore().collection('users').doc(`${props.route.params.userInfo.id}`).update({
+                    firstname: firstname,
+                    lastname: lastname, 
+                    username: username, 
+                    schoolname: schoolname
+                }).then(() => {
+                    console.log(`updated details for the user with email ${props.route.params.userInfo.email}`);
+                }), 
                 style: "destructive"
             }
         ]
@@ -36,7 +61,7 @@ const SettingsPage = (props) => {
 
     return (
         <View style={{ flex: 1, alignItems: 'center', margin: 20}}>
-            <Text style={{ fontSize: 30, marginBottom: 20 }}>Hello, {name}!</Text>
+            <Text style={{ fontSize: 30, marginBottom: 20 }}>Hello, {firstname}!</Text>
             <Image
                 style={{width: 150, height: 150, borderRadius: 75}} 
                 source={require('../../assets/user_icon.png')}
@@ -66,22 +91,20 @@ const SettingsPage = (props) => {
                 onChangeText={setUserName}
             />
             <Input
-                placeholder="School"
+                placeholder="Schoolname"
                 clearButtonMode='always'
-                value={school}
-                onChangeText={setSchool}
+                value={schoolname}
+                onChangeText={setSchoolName}
             />
             <Input
                 rightIcon={{ type: 'font-awesome', name: 'toggle-on', color: 'green', size: 35 }}
                 value={'Allow others to find you'}
                 disabled= 'true'
-                onChangeText={setSchool}
             />
             <Input
                 rightIcon={{ type: 'font-awesome', name: 'toggle-on', color: 'green', size: 35 }}
                 value={'Allow push notifications'}
                 disabled= 'true'
-                onChangeText={setSchool}
             />
             <Button
                 title="Save Changes"
